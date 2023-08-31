3c04a5e164fc81db10a4ad266b67f6ea37dff9ca
Dawid Wiszniewski <cherrz@MacBook-Air-Dawid.local>
2023-08-31 20:25:33 +0200

    adding all products to cart

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -26,7 +26,7 @@ test('Adding all products to cart', async ({ page }) => {
   await page.getByPlaceholder('Username').fill(login); // enter login into field
   await page.getByPlaceholder('Password').fill(passsword); //enter password into field
   await page.locator('#login-button').click(); // click on the submit button
-  const productButtons = page.locator('.btn_inventory');
+  const productButtons = page.locator('.btn_inventory');//locate all add to card buttons
 
   for (let i = 0; i < await productButtons.count(); i++) {
     await productButtons.nth(i).click();
